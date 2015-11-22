7ac63df8516fa07d8b929318b78ebec9d03640e2
Erendis Near <mailbox.novaka@gmail.com>
2015-11-22 10:14:44 +0100

    Create Cell.cs

---
+++ Cell.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace KC12VU_CGoL
+{
+    class Cell : Button
+    {
+        public Cell(int size, int thickness, int row, int col, Brush alive, Brush dead, Brush border)
+        {
+            Width = size;
+            Height = size;
+            BorderBrush = border;
+            BorderThickness = new Thickness(thickness);
+            nbrs = 0;
+            next = false;
+            this.row = row;
+            this.col = col;
+            this.alive = alive;
+            this.dead = dead;
+            this.border = border;
+            Living = false;
+        }
+
+        Brush alive;
+        Brush dead;
+        Brush border;
+
+        bool living;
+
+        public bool Living
+        {
+            get { return living; }
+            set
+            {
+                living = value;
+                Background = living ? alive : dead;
+            }
+        }
+
+        int nbrs;
+
+        public int Nbrs
+        {
+            get { return nbrs; }
+            set { nbrs = value; }
+        }
+
+        bool next;
+
+        public bool Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        int row;
+
+        public int Row
+        {
+            get { return row; }
+        }
+
+        int col;
+
+        public int Col
+        {
+            get { return col; }
+        }
+    }
+}
